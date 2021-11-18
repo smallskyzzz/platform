f8399f1e4faa343318cd6283677ca0aa1163cce2
tiansheng.zhu <tiansheng.zhu@amh-group.com>
2021-11-18 15:12:33 +0800

    feat eslint设置

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  // globals: {
+  //   AMap: true,
+  //   AMapUI: true,
+  //   TmsVersion: true,
+  //   IsLocal: true
+  // },
+  rules: {
+    "no-unused-vars": [2, {
+      "vars": "local",
+      "args": "none"
+    }],
+    'vue/no-reserved-component-names' : 'off',
+    'array-callback-return': 'off',
+    'no-return-assign': 'off',
+    'no-param-reassign': 'off',
+    'no-inner-declarations': 'off',
+    'linebreak-style': [0, 'error', 'windows'],
+    'default-case-last' : 'off',
+    'no-loss-of-precision': 'off',
+    'no-promise-executor-return': 'off',
+    'no-unreachable-loop': 'off',
+    'no-useless-backreference': 'off',
+    'complexity': 'off',
+    'space-before-function-paren': 'off',
+    'arrow-parens': 'off',
+    'max-nested-callbacks':'off'
+  },
+}

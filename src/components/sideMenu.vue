00979970a297e334425a3ff40975581e091b17fc
tiansheng.zhu <tiansheng.zhu@amh-group.com>
2021-11-22 17:38:08 +0800

    feat 导航默认选中项

--- src/components/sideMenu.vue
+++ src/components/sideMenu.vue
@@ -2,6 +2,7 @@
   <div>
     <h5 style="height: 60px;margin: 0">logo</h5>
     <el-menu
+        :default-active="activeName"
         style="height: calc(100% - 60px)"
         background-color="rgba(0, 21, 41, 1)"
         text-color="rgba(255, 255, 255, .6)"
@@ -34,8 +35,7 @@ export default {
   },
   computed: {
     activeName () {
-      console.log(this.routers.findIndex((r) => r.path === this.$router.path))
-      return this.routers.findIndex((r) => r.path === this.$router.path)
+      return this.routers.findIndex((r) => r.path === this.$route.path) + ''
     }
   },
   created () {
